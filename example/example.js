8292fa3f68ea03e08ed8b8f72535f17151706891
lujiezhao <350012709@qq.com>
2023-10-13 09:57:52 +0800

    feat:utils init
    --story=init

---
+++ example/example.js
@@ -0,0 +1,7 @@
+/**
+ * node 引用示例
+ */
+const { random } = require('dip-utils')
+console.log(random(1, 3))
+
+// 终端执行命令：node example.js
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 统一导出所有函数
+ */
+export * from './main/main'
+export * from './main/date/date'
+export * from './main/is/is'
+export * from './main/url/url'
+export * from './main/store/store'
