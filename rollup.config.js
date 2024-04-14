6a1a2f9d102c83702abd7e8db45dc09083edd317
lujiezhao <350012709@qq.com>
2024-04-14 18:18:12 +0800

    feat: change jason
    --story=0414

--- rollup.config.js
+++ rollup.config.js
@@ -6,17 +6,17 @@ export default {
   output: [
     {
       format: 'cjs', // 打包为commonjs格式
-      file: 'dist/lmkj-utils.cjs.js', // 打包后的文件路径名称
+      file: 'dist/jason-utils.cjs.js', // 打包后的文件路径名称
       name: 'dutils' // 打包后的默认导出文件名称
     },
     {
       format: 'esm', // 打包为esm格式
-      file: 'dist/lmkj-utils.esm.js',
+      file: 'dist/jason-utils.esm.js',
       name: 'dutils'
     },
     {
       format: 'umd', // 打包为umd通用格式
-      file: 'dist/lmkj-utils.umd.js',
+      file: 'dist/jason-utils.umd.js',
       name: 'dutils',
       minifyInternalExports: true
     }
